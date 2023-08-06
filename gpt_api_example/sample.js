a4513c38f6e0585480ada8202e8b9373418c768c
casang <carlos.sang@gmail.com>
2023-08-06 15:39:38 -0300

    v1

---
+++ gpt_api_example/sample.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+const API_ENDPOINT = 'https://api.openai.com/v1/engines/text-davinci-003/completions'; // Replace with the appropriate API endpoint
+const API_KEY = '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'; // Replace with your GPT-3.5 API key
+
+async function generateText(prompt) {
+  try {
+    const response = await axios.post(
+      API_ENDPOINT,
+      {
+        prompt,
+        max_tokens: 100, // Adjust this value based on the desired length of the generated text
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      }
+    );
+
+    return response.data.choices[0].text.trim();
+  } catch (error) {
+    console.error('Error generating text:', error.message);
+    return null;
+  }
+}
+
+async function main() {
+  const prompt = 'Once upon a time'; // Replace with your desired prompt
+
+  const generatedText = await generateText(prompt);
+  if (generatedText) {
+    console.log('Generated Text:');
+    console.log(generatedText);
+  }
+}
+
+
+main();
